01c52b141c0caebd7dda3c5f480126db15cf027d
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2023-02-10 23:27:39 -0700

    Time: 175 ms (54.85%), Space: 15.4 MB (79.61%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,15 +1,17 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums)
-        mySet = set(nums)
-        maxvalue = 0
-        maxelement = 0
-        countOfChars = defaultdict(int)
-        for element in mySet:
-            countOfChar = nums.count(element)
-            countOfChars[element] = countOfChar
-            if countOfChars[element] > n/2 and countOfChars[element] > maxvalue:
-                maxvalue=countOfChars[element]
-                maxelement = element
+        nums.sort()
+        return nums[len(nums)//2]
+#         n = len(nums)
+#         mySet = set(nums)
+#         maxvalue = 0
+#         maxelement = 0
+#         countOfChars = defaultdict(int)
+#         for element in mySet:
+#             countOfChar = nums.count(element)
+#             countOfChars[element] = countOfChar
+#             if countOfChars[element] > n/2 and countOfChars[element] > maxvalue:
+#                 maxvalue=countOfChars[element]
+#                 maxelement = element
 
-        return maxelement
+#         return maxelement
