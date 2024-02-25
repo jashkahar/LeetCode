226781150a6cd39d6d82cc577ae037b65d88d4e3
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-02-24 23:37:52 -0700

    Time: 50 ms (10.14%), Space: 16.7 MB (48.13%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,48 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        
+        lt = list(t)
+        ls = list(s)
+        
+        n = len(ls)
+        m = len(lt)
+        
+        i , j = 0, 0
+        
+        if n == 0:
+            return True
+        if m == 0:
+            return False
+        
+        while j< m:
+            print("searching", ls[i])
+            print("current j:", lt[j])
+            if ls[i] == lt[j]:
+                print("Found")
+                i += 1
+                j += 1
+                if i == n:
+                    return True
+            else:
+                j += 1
+        return False
+
+        
+        
+        
+            
+        
+        
+        
+#         newt = set(t)
+#         news = list(s)
+        
+#         for i in news:
+#             if i in newt:
+#                 pass
+#             else:
+#                 return False
+            
+#         return True
+        
+        
