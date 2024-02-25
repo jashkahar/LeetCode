477bae1a742386ae81575931f4f4373de58f141a
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-02-24 20:41:30 -0700

    Time: 1612 ms (6.55%), Space: 35.5 MB (5.05%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -0,0 +1,45 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:  
+        n = len(nums)
+        
+        if n < 3:
+            return False
+        
+        leftmin = [None]*n
+        leftmin[0] = nums[0]
+        for i in range(1,n):
+            leftmin[i] =min(leftmin[i-1], nums[i])
+        print('Leftmin', leftmin)
+        
+        
+        rightmax = [None]*n
+        rightmax[n-1] = nums[n-1]
+        for i in range(n-2,-1,-1):
+            rightmax[i] = max(rightmax[i+1], nums[i])
+        print('Rightmax', rightmax)
+        
+        for i in range(n):
+            print(leftmin[i], nums [i], rightmax[i])
+            if(leftmin[i] < nums[i] and nums[i] < rightmax[i]):
+                return True
+        return False
+
+        
+#         mini = min(nums)
+#         maxi = max(nums)
+#         print('min:',mini)
+#         print('max:',maxi)
+        
+#         if if(nums.index(maxi)<nums.index(mini)):
+            
+
+#         if(nums.index(maxi)>nums.index(mini)):
+#             for temp in nums[mini:maxi]:
+#                 print(temp)
+#                 if(mini<temp<maxi):
+                    
+#                     return True
+#                 else:
+#                     return False
+                
+            
