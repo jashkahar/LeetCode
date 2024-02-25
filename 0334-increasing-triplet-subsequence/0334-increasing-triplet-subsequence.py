47f9a0d18e713db59c6dc8493200d03abeaae459
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-02-24 21:18:21 -0700

    Time: 996 ms (17.86%), Space: 32.5 MB (97.67%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -9,17 +9,17 @@ class Solution:
         leftmin[0] = nums[0]
         for i in range(1,n):
             leftmin[i] =min(leftmin[i-1], nums[i])
-        print('Leftmin', leftmin)
+        #print('Leftmin', leftmin)
         
         
         rightmax = [None]*n
         rightmax[n-1] = nums[n-1]
         for i in range(n-2,-1,-1):
             rightmax[i] = max(rightmax[i+1], nums[i])
-        print('Rightmax', rightmax)
+        #print('Rightmax', rightmax)
         
         for i in range(n):
-            print(leftmin[i], nums [i], rightmax[i])
+            #print(leftmin[i], nums [i], rightmax[i])
             if(leftmin[i] < nums[i] and nums[i] < rightmax[i]):
                 return True
         return False
