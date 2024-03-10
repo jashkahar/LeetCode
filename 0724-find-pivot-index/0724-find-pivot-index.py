e11aeda1f6159a31a2ed7620c0aac9219861dc44
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-03-09 17:29:35 -0700

    Time: 126 ms (48.07%), Space: 17.7 MB (74.01%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -3,12 +3,8 @@ class Solution:
         l = 0
         k = sum(nums)
         
-        # if k == 0:
-        #     return -1
-        # k -=nums[0]
-        
         for r,i in enumerate(nums):
-            print(l,i,k-i)
+            # print(l,i,k-i)
             if(l == k-i):
                 return r
             l+=i
