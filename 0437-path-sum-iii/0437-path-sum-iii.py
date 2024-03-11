00707bdcc06f074c2ab59a6a7a629a88392115ff
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-03-10 17:54:26 -0700

    Time: 48 ms (70.43%), Space: 16.9 MB (66.71%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -8,26 +8,44 @@ class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         
         self.total = 0
-        
-        def helper(node, cur):
+        self.lookup = defaultdict(int)
+        self.lookup[targetSum] = 1
+            
+        def dfs(node, rootsum):
             if not node:
                 return
-            helper(node.left, cur+node.val)
-            helper(node.right, cur+node.val)
-            # print(cur +node.val)
-            if cur + node.val == targetSum:
-                self.total += 1
-                # print('hello')
+            rootsum +=node.val
+            self.total += self.lookup[rootsum]
+            self.lookup[rootsum + targetSum] +=1
+            dfs(node.left, rootsum)
+            dfs(node.right, rootsum)
+            self.lookup[rootsum+targetSum] -=1
             
-        def dfs(node):
-            # print('hi')
-            if not node:
-                return
-            helper(node, 0)
-            dfs(node.left)
-            dfs(node.right)
-            
-        dfs(root)
+        dfs(root,0)
         
         return self.total
+        
+#         self.total = 0
+        
+#         def helper(node, cur):
+#             if not node:
+#                 return
+#             helper(node.left, cur+node.val)
+#             helper(node.right, cur+node.val)
+#             # print(cur +node.val)
+#             if cur + node.val == targetSum:
+#                 self.total += 1
+#                 # print('hello')
+            
+#         def dfs(node):
+#             # print('hi')
+#             if not node:
+#                 return
+#             helper(node, 0)
+#             dfs(node.left)
+#             dfs(node.right)
+            
+#         dfs(root)
+        
+#         return self.total
         
