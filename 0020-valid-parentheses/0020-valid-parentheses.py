163edcc2dc68c2a9300338b67cd9ae169b25ad65
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-03-13 08:01:56 -0700

    Time: 41 ms (20.32%), Space: 16.6 MB (32.46%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,52 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        #print(len(s))
+        #print(10**4)
+        if (len(s) >= 10**4):
+            return False
+        temp_list = list(s)
+        
+        stack = []
+        
+        for i in range(len(temp_list)):
+            #print("element added: ",temp_list[i])
+            if(temp_list[i] in ('(' ,'{','[' )):
+                stack.append(temp_list[i])
+                #print("current stack: ",stack)
+            elif(temp_list[i] in (')','}' ,']' )):
+                #print("-Closing bracket detected-")
+                if(stack == []):
+                    return False
+                elif(temp_list[i] == ')'):
+                    if(stack[-1] == '('):
+                        stack.pop()
+                        #print("Element () removed" )
+                    else:
+                        return False
+                elif(temp_list[i] == '}'):
+                    if(stack[-1] == '{'):
+                        stack.pop()
+                        #print("Element {} removed" )
+                    else:
+                        return False    
+                elif(temp_list[i] == ']'):
+                    if(stack[-1] == '['):
+                        stack.pop()
+                        #print("Element [] removed" )
+                    else:
+                        return False
+            i=i+1
+            
+        # print("after process",stack)
+        if(stack == []):
+            return True
+        else:
+            return False
+                    
+        
+        
+        
+    
+        
+        
+        
