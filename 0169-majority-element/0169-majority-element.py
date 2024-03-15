657cf68e7f4837608fc4d80f5c7e42622af5589e
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-03-14 18:17:42 -0700

    Time: 152 ms (37.27%), Space: 17.9 MB (78.95%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        c = dict(Counter(nums))
-        print(c)
-        sorted_c = dict(sorted(c.items(), key = lambda x: x[1], reverse = True ))
-        lc = list(sorted_c)
-
-        print(lc)
-        return lc[0]
+        return list(dict(sorted(dict(Counter(nums)).items(), key = lambda x: x[1], reverse = True )))[0]
+        # return lc[0]
         
