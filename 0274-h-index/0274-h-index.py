d718084f828d3a732c76491b976a5dfdcc6241d1
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-03-15 20:50:03 -0700

    Time: 35 ms (89.06%), Space: 16.9 MB (77.17%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,25 +1,19 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        N = len(citations)
-        tmp = [0 for a in range(N+1)]
+        n = len(citations)
+        tmp = [ 0 for a in range(n+1) ]
         
         for i,v in enumerate(citations):
-            if v>N:
-                tmp[N] += 1
+            if v > n:
+                tmp[n] += 1
             else:
                 tmp[v] += 1
-        total = 0
-        for i in range(N, -1, -1):
+        total = 0    
+        print(tmp)
+        for i in range(n,-1,-1):
             total += tmp[i]
             if total >= i:
                 return i
-#         Slower
-#         N= len(citations)
-        
-#         citations.sort()
-        
-#         for i,v in enumerate(citations):
-#             if N-i <= v:
-#                 return N-i
-#         return 0
+        return 0
+
         
