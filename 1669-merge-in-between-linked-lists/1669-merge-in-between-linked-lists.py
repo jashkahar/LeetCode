df4e3289c6797c361d315f5ea317d61da53007f7
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-03-20 14:09:39 -0700

    Time: 184 ms (86.12%), Space: 21 MB (70.65%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.py
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.py
@@ -12,28 +12,22 @@ class Solution:
         end = ListNode()
         nextstart = ListNode()
         
-        
         c = 0
         
         while c<b:
             if c == a-1:
                 start = list1
-            # if c == b:
-            #     end = list1
-            #     nextstart = list1.next
             c+=1
             list1 = list1.next
         start.next = list2
         
         while list2:
             if not list2.next:
-                print(list2.val)
+                # print(list2.val)
                 list2.next = list1.next
                 break
-            list2 = list2.next
-            
-        print(list1.val)
-        
+            list2 = list2.next            
+        # print(list1.val)
         # list2 = list1.next
         list1.next = None
         
