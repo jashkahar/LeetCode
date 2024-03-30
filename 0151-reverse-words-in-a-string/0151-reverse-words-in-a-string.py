bed8002d620b0d729e57bc9dc47d5e33d13bea54
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-03-30 14:30:50 -0700

    Time: 48 ms (6.43%), Space: 16.9 MB (8.93%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,33 +1,33 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        x = s.split()
-        print(x)
-        return " ".join(x[::-1])
-#         l = s.split(' ')
-#         rev=[]
-#         word=0
-#         space=0
-#         for i in reversed(l):
-#             # print(i)
-#             # if i=='':
-#                 # print('blank space')
-#             if(i=='' and word == 0 and space == 0):
-#                 print('Blank space not needed', word, space)
-#                 word=0
-#                 space=1
-#             elif(i=='' and word == 0 and space == 1):
-#                 print('Blank space not needed', word, space)
-#                 word = 0
-#                 space=1
-#             elif(i=='' and word == 1 and space == 0):
-#                 print('Blank space not needed', word, space)
-#                 # rev.append(i)
-#                 word = 0
+        # x = s.split()
+        # print(x)
+        # return " ".join(x[::-1])
+        l = s.split(' ')
+        rev=[]
+        word=0
+        space=0
+        for i in reversed(l):
+            # print(i)
+            # if i=='':
+                # print('blank space')
+            if(i=='' and word == 0 and space == 0):
+                print('Blank space not needed', word, space)
+                word=0
+                space=1
+            elif(i=='' and word == 0 and space == 1):
+                print('Blank space not needed', word, space)
+                word = 0
+                space=1
+            elif(i=='' and word == 1 and space == 0):
+                print('Blank space not needed', word, space)
+                # rev.append(i)
+                word = 0
                 
-#             else:
-#                 print(i,' -->word', word, space)
-#                 rev.append(i)
-#                 word=1
-#                 space=0
-#         return ' '.join(rev)
-#         # return rev
+            else:
+                print(i,' -->word', word, space)
+                rev.append(i)
+                word=1
+                space=0
+        return ' '.join(rev)
+        # return rev
