c81000a3a871b2a2e260bd6e54fd1d058b4c611b
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-03-31 11:57:43 -0700

    Time: 170 ms (32.80%), Space: 18.1 MB (35.16%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,5 +1,28 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return list(dict(sorted(dict(Counter(nums)).items(), key = lambda x: x[1], reverse = True )))[0]
-        # return lc[0]
+        # O(1) space solution using boyer moore algorithm
+        res, count = 0,0
+        
+        for n in nums:
+            if count == 0:
+                res = n
+            count += (1 if n == res else -1)
+        return res
+
+
+        
+#         ----Hashmap Solution---
+#         count = {}
+        
+#         res, maxCount = 0,0
+        
+#         for n in nums:
+#             count[n] = 1 +count.get(n,0)
+#             res = if count[n] > maxCount else res
+#             maxCount = max(count[n], maxCount)
+#         return res
+
+
+#       ----Fancy hashmap colution ----
+        # return list(dict(sorted(dict(Counter(nums)).items(), key = lambda x: x[1], reverse = True )))[0]
         
