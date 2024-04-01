d08e363c71671ed7304b9003cdcd186821830739
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-04-01 16:15:03 -0700

    Time: 63 ms (97.74%), Space: 18.1 MB (24.48%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -6,19 +6,19 @@ class Solution:
                 nums[l] = nums[r]
                 l+=1
         return l
-#         seen = set()
+        # seen = set()
         
-#         for i, n in enumerate(nums):
-#             if nums[i] not in seen:
-#                 seen.add(n)
-#             elif nums[i] in seen:
-#                 nums[i] = float('inf')
-#                 nums.sort()
-#                 # print(nums)
-#             else:
-#                 # print(nums)
-#                 nums.sort()
-#                 return i
-#         return len(seen)
+        # for i, n in enumerate(nums):
+        #     if nums[i] not in seen:
+        #         seen.add(n)
+        #     elif nums[i] in seen:
+        #         nums[i] = float('inf')
+        #         nums.sort()
+        #         # print(nums)
+        #     else:
+        #         # print(nums)
+        #         nums.sort()
+                # return i
+        # return len(seen)
         
         
