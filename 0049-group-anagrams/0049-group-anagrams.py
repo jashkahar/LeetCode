949cded8479ff997bfcec8532eda7cd8fa6743c8
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-08-23 11:13:52 -0700

    Time: 106 ms (11.98%), Space: 22.1 MB (19.58%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,6 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # anagrams = defaultdict(list)
         
+        # for char in strs:
+        #     # print(sorted(set(char)))
+            
+        #     anagrams["".join(sorted(set(char)))].append(char)
+        # print(sorted(anagrams.values()))
+        # return anagrams       
         res = defaultdict(list)
         
         for s in strs:
@@ -8,6 +15,7 @@ class Solution:
             
             for c in s:
                 count[ord(c) - ord("a")] += 1
+            
             
             res[tuple(count)].append(s)
         return res.values()
