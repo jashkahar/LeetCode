a8e1837d5d83cc5ca232c26ecb6f20fafd9c49f4
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-09-03 14:22:07 -0700

    Time: 41 ms (19.81%), Space: 16.4 MB (69.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,5 +3,8 @@ class Solution:
         rob1, rob2 = 0,0
         
         for n in nums:
-            rob1, rob2 = rob2, max(n +rob1, rob2)
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
         return rob2
+        
