24d28278886bfceb2d6f94b0f4dd975a8391d90a
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-09-05 23:46:01 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,21 +1,20 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = defaultdict(list)
+        # anagrams = defaultdict(list)
         
-        for word in strs:
-            # Sort the characters of the word to create a key
-            key = tuple(sorted(word))
-            anagrams[key].append(word)
+        # for word in strs:
+        #     key = tuple(sorted(word))
+        #     anagrams[key].append(word)
+        # return list(anagrams.values()) 
+
+
+        res = defaultdict(list)
         
-        return list(anagrams.values())      
-        # res = defaultdict(list)
-        
-        # for s in strs:
-        #     count = [0] * 26
-            
-        #     for c in s:
-        #         count[ord(c) - ord("a")] += 1
-        #     res[tuple(count)].append(s)
-        # print(count)
-        # return res.values()
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            res[tuple(count)].append(s)
+            print(res)
+        return res.values()
         
