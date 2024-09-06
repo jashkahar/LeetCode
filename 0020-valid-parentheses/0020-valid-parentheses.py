00316194f8aae1c536768ae478c288c74feb463d
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-09-05 21:24:42 -0700

    Time: 27 ms (94.41%), Space: 16.7 MB (10.48%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,43 +1,57 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         temp_list = list(s)
+
+        parathesis = {']':'[', '}':'{', ')':'('}
         
         stack = []
-        
-        for i in range(len(temp_list)):
-            #print("element added: ",temp_list[i])
-            if(temp_list[i] in ('(' ,'{','[' )):
-                stack.append(temp_list[i])
-                #print("current stack: ",stack)
-            elif(temp_list[i] in (')','}' ,']' )):
-                #print("-Closing bracket detected-")
-                if(stack == []):
+
+        for char in s:
+            if char in ["[", "{", '(']:
+                stack.append(char)
+            else:
+                if stack and parathesis[char] == stack[-1]:
+                    stack.pop()
+                else:
                     return False
-                elif(temp_list[i] == ')'):
-                    if(stack[-1] == '('):
-                        stack.pop()
-                        #print("Element () removed" )
-                    else:
-                        return False
-                elif(temp_list[i] == '}'):
-                    if(stack[-1] == '{'):
-                        stack.pop()
-                        #print("Element {} removed" )
-                    else:
-                        return False    
-                elif(temp_list[i] == ']'):
-                    if(stack[-1] == '['):
-                        stack.pop()
-                        #print("Element [] removed" )
-                    else:
-                        return False
-            i=i+1
-            
-        # print("after process",stack)
-        if(stack == []):
+        if not stack:
             return True
-        else:
-            return False
+        return False
+        
+        # for i in range(len(temp_list)):
+        #     #print("element added: ",temp_list[i])
+        #     if(temp_list[i] in ('(' ,'{','[' )):
+        #         stack.append(temp_list[i])
+        #         #print("current stack: ",stack)
+        #     elif(temp_list[i] in (')','}' ,']' )):
+        #         #print("-Closing bracket detected-")
+        #         if(stack == []):
+        #             return False
+        #         elif(temp_list[i] == ')'):
+        #             if(stack[-1] == '('):
+        #                 stack.pop()
+        #                 #print("Element () removed" )
+        #             else:
+        #                 return False
+        #         elif(temp_list[i] == '}'):
+        #             if(stack[-1] == '{'):
+        #                 stack.pop()
+        #                 #print("Element {} removed" )
+        #             else:
+        #                 return False    
+        #         elif(temp_list[i] == ']'):
+        #             if(stack[-1] == '['):
+        #                 stack.pop()
+        #                 #print("Element [] removed" )
+        #             else:
+        #                 return False
+        #     i=i+1
+            
+        # # print("after process",stack)
+        # if(stack == []):
+        #     return True
+        # else:
+        #     return False
                     
         
         
