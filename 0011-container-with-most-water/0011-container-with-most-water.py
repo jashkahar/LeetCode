5fad78ea86cdf7375a16c6a17816b0a3ab0736c2
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-09-06 23:40:12 -0700

    Time: 537 ms (20.72%), Space: 29.8 MB (25.77%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,15 +4,12 @@ class Solution:
         left = 0
         right = len(height)-1
         maxx = 0
-        while(left<right):
-            currmax = (min(height[left],height[right]) * (right - left))
-            
-            if(maxx<currmax):
-                maxx = currmax
-            if(height[left]<height[right]):
+        while left<right:
+            currmax = min(height[left], height[right]) * (right - left)
+            maxx = max(currmax, maxx)
+
+            if(height[left] < height[right]):
                 left += 1
             else:
                 right -= 1
-                
-            
         return maxx
