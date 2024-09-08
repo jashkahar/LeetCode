b776a0c0a5af351db936a9621cc16b058d1be3d7
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-09-07 22:33:30 -0700

    Time: 40 ms (31%), Space: 16.4 MB (80%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -2,6 +2,8 @@ class Solution:
     def reverse(self, x: int) -> int:
         MIN = -2**31 #-2147483648
         MAX = 2**31-1 #2147483647
+        print(MAX // 10)
+        print(MAX%10)
 
         res = 0
 
@@ -9,8 +11,8 @@ class Solution:
             digit = int(math.fmod(x, 10))
             x = int(x/10)
 
-            if (res > MAX // 10 or 
-               (res == MAX // 10 and digit >= MAX % 10)):
+            if (res > 214748364 or 
+               (res == 214748364 and digit >= 7 )):
                 return 0
             if (res < MIN // 10 or 
                (res == MIN // 10 and digit <= MIN % 10)):
