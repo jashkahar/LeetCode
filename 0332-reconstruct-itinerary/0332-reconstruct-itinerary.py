0286b2b3d26b460b7f6838a40336f70666079db6
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-10-15 17:54:25 -0700

    Time: 72 ms (75.79%), Space: 17.1 MB (17.64%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,27 +1,27 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        adj  = { src: [] for src, dst in tickets}
-
-        tickets.sort()
-
+        adj = defaultdict(list)
+        
+        # Sort tickets in reverse order and construct the adjacency list
+        tickets.sort(reverse=True)
         for src, dst in tickets:
             adj[src].append(dst)
         
         res = ["JFK"]
 
         def dfs(src):
-            if len(res)== len(tickets)+1:
-                return True
-            if src not in adj:
-                return False
-            
-            temp = list(adj[src])
-            for i, v in enumerate(temp):
-                adj[src].pop(i)
-                res.append(v)
-                if dfs(v): return True
-                adj[src].insert(i,v)
-                res.pop()
-            return False
+            while adj[src]:
+                # Always pick the last element because we've sorted in reverse order
+                next_dest = adj[src].pop()
+                dfs(next_dest)
+            res.append(src)
+        
+        # Start DFS from JFK
         dfs("JFK")
+        
+        # The result list is constructed in reverse order, so we need to reverse it
+        
+        res = res[::-1]
+
+        res.pop()
         return res
