c630d0fbc35030a44f932e48f078e8fd10c4364d
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-10-19 12:58:35 -0700

    Time: 25 ms (99.4%), Space: 22.4 MB (99.45%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,5 +1,17 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        """
+        1, 2, 3, 4
+        1, 1, 2, 6
+        24, 12, 4, 1
+
+        ipt = 1, 2, 3, 4
+        prefix = 24
+        postfix = 24
+        res = 24, 12, 8, 6 
+
+        """
         
         prefix = 1
         postfix = 1
