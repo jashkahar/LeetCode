ccabf5501d1a3eda8958492d4aaed6eebfe5c8d9
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2024-10-18 19:09:10 -0700

    Time: 47 ms (100%), Space: 16.7 MB (11.58%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,12 +1,10 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         count = defaultdict(int)
-
         res = 0
-
         l = 0
-
         maxf = 0
+
         for r in range(len(s)):
             count[s[r]] += 1
             maxf = max(maxf, count[s[r]])
@@ -15,4 +13,4 @@ class Solution:
                 count[s[l]] -= 1
                 l += 1
             res = max(res, r-l+1)
-        return res
+        return res 
