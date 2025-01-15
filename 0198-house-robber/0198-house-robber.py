607509a1fd4a5cb993c7c5146f6ea43dc9c2f717
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2025-01-14 19:47:25 -0700

    Time: 0 ms (100%), Space: 17.7 MB (25.96%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,10 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1, rob2 = 0,0
+        prev1, prev2 = 0,0
+        for num in nums:
+            temp = prev1
+            prev1 = max(prev1, prev2 + num)  # Update the current max
+            prev2 = temp  # Update the second last value
         
-        for n in nums:
-            temp = max(n + rob1, rob2)
-            rob1 = rob2
-            rob2 = temp
-        return rob2
+        return prev1
         
